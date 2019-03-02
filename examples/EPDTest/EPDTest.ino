06ae88243fd955a5ecc5818141be264b702206f0
ladyada <limor@ladyada.net>
2019-03-02 02:55:49 -0500

    support 2.13" flexible monochrome

--- examples/EPDTest/EPDTest.ino
+++ examples/EPDTest/EPDTest.ino
@@ -16,28 +16,46 @@
 #define EPD_BUSY    3 // can set to -1 to not use a pin (will wait a fixed delay)
 
 /* Uncomment the following line if you are using 1.54" tricolor EPD */
-Adafruit_IL0373 display(152, 152 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//Adafruit_IL0373 display(152, 152 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
-/* Uncomment the following line if you are using 2.15" tricolor EPD */
-//Adafruit_IL0373 display(212, 104 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+/* Uncomment the following line if you are using 2.13" tricolor EPD */
+Adafruit_IL0373 display(212, 104 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//#define FLEXIBLE_213
 
-/* Uncomment the following line if you are using 2.7" tricolor EPD */
+/* Uncomment the following line if you are using 2.7" tricolor or grayscale EPD */
 //Adafruit_IL91874 display(264, 176 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS);
 
+#if defined(FLEXIBLE_213)
+  #define COLOR1 EPD_RED
+  #define COLOR2 EPD_RED
+#else // any other tricolor
+  #define COLOR1 EPD_BLACK
+  #define COLOR2 EPD_RED
+#endif
+
 void setup() {
+  Serial.begin(115200);
+ //while (!Serial) { delay(10); }
+  Serial.println("Adafruit EPD test");
+  
   display.begin();
+  
+#if defined(FLEXIBLE_213)
+  display.invertColorLogic(0, false);
+  display.invertColorLogic(1, false);
+#endif
+
   display.clearBuffer();
 
+  
   //draw some pretty lines
   for (int16_t i=0; i<display.width(); i+=4) {
-    display.drawLine(0, 0, i, display.height()-1, EPD_BLACK);
+    display.drawLine(0, 0, i, display.height()-1, COLOR1);
   }
 
   for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, EPD_RED);
+    display.drawLine(display.width()-1, 0, 0, i, COLOR2);  // on grayscale this will be mid-gray
   }
-  
-  //refresh the display
   display.display();
 }
 
