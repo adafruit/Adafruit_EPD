1c246a71cdb7f0524dee7ff2f3f36aaab81fe223
ladyada <limor@ladyada.net>
2019-03-30 14:43:40 -0400

    add more displays

--- examples/rotation_test/rotation_test.ino
+++ examples/rotation_test/rotation_test.ino
@@ -17,14 +17,29 @@
 #define SRAM_CS     6
 #define EPD_RESET   4 // can set to -1 and share with microcontroller Reset!
 #define EPD_BUSY    -1 // can set to -1 to not use a pin (will wait a fixed delay)
+
 /* Uncomment the following line if you are using 1.54" tricolor EPD */
-//Adafruit_IL0373 epd(152, 152 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//Adafruit_IL0373 display(152, 152, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
-/* Uncomment the following line if you are using 2.15" tricolor EPD */
-//Adafruit_IL0373 epd(212, 104 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+/* Uncomment the following line if you are using 1.54" monochrome EPD */
+//Adafruit_SSD1608 display(200, 200, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
-/* Uncomment the following line if you are using 2.7" tricolor EPD */
-Adafruit_IL91874 epd(264, 176 ,EPD_DC, EPD_RESET, EPD_CS, SRAM_CS);
+/* Uncomment the following line if you are using 2.13" tricolor EPD */
+Adafruit_IL0373 display(212, 104, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//#define FLEXIBLE_213
+
+/* Uncomment the following line if you are using 2.13" monochrome 250*122 EPD */
+//Adafruit_SSD1675 display(250, 122, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+
+/* Uncomment the following line if you are using 2.7" tricolor or grayscale EPD */
+//Adafruit_IL91874 display(264, 176, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS);
+
+/* Uncomment the following line if you are using 2.9" EPD */
+//Adafruit_IL0373 display(296, 128, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//#define FLEXIBLE_290
+
+/* Uncomment the following line if you are using 4.2" tricolor EPD */
+//Adafruit_IL0398 display(300, 400, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
 
 void setup(void) {
