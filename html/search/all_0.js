1040664c7fc931b2b7e7a7de5c1b2323d366ae95
Travis CI <travis@travis-ci.org>
2019-04-10 03:36:47 +0000

    Deploy code docs to GitHub Pages Travis build: 90
    Commit: 5336a16618f2719cfb63fd68371bb0566c7a1fce

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['_5fbusy_5fpin',['_busy_pin',['../class_adafruit___e_p_d.html#a2dd5b241d38b2129a5fd3e6e3b06ee68',1,'Adafruit_EPD']]],
   ['_5fcs_5fpin',['_cs_pin',['../class_adafruit___e_p_d.html#a70525641be2a740700bb32ee2c8aea04',1,'Adafruit_EPD']]],
   ['_5fdc_5fpin',['_dc_pin',['../class_adafruit___e_p_d.html#a7037610b393b33da55c53ad00ea37465',1,'Adafruit_EPD']]],
+  ['_5fisintransaction',['_isInTransaction',['../class_adafruit___e_p_d.html#aecdfd00801652649598de6bfd05a83d4',1,'Adafruit_EPD']]],
   ['_5freset_5fpin',['_reset_pin',['../class_adafruit___e_p_d.html#aaae064664f466dcbb4996f6dd5199b6e',1,'Adafruit_EPD']]],
   ['_5fsclk_5fpin',['_sclk_pin',['../class_adafruit___e_p_d.html#a4f165c405539b3f02af6c87631340e75',1,'Adafruit_EPD']]],
   ['_5fsid_5fpin',['_sid_pin',['../class_adafruit___e_p_d.html#ab2f5a3c7444c3f00974409d7b6f06a1c',1,'Adafruit_EPD']]]
