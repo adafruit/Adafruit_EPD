d656bb6df129cbe7ade4cfd7be62514c163b217f
lady ada <limor@ladyada.net>
2020-09-27 12:39:19 -0400

    cleaning up file locaitons

--- src/Adafruit_EPD.h
+++ src/Adafruit_EPD.h
@@ -173,12 +173,12 @@ protected:
   void dcLow();
 };
 
-#include "Adafruit_IL0373.h"
-#include "Adafruit_IL0398.h"
-#include "Adafruit_IL91874.h"
-#include "Adafruit_SSD1608.h"
-#include "Adafruit_SSD1675.h"
-#include "Adafruit_SSD1675B.h"
-#include "Adafruit_SSD1680.h"
+#include "drivers/Adafruit_IL0373.h"
+#include "drivers/Adafruit_IL0398.h"
+#include "drivers/Adafruit_IL91874.h"
+#include "drivers/Adafruit_SSD1608.h"
+#include "drivers/Adafruit_SSD1675.h"
+#include "drivers/Adafruit_SSD1675B.h"
+#include "drivers/Adafruit_SSD1680.h"
 
 #endif /* _ADAFRUIT_EPD_H_ */
