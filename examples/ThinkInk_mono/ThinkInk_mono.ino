f5b8c796ecb76110bcbd3d56ed18b2668e7c81df
lady ada <limor@ladyada.net>
2020-10-20 18:58:17 -0400

    add many more panesl

--- examples/ThinkInk_mono/ThinkInk_mono.ino
+++ examples/ThinkInk_mono/ThinkInk_mono.ino
@@ -9,47 +9,45 @@
 
 #include "Adafruit_ThinkInk.h"
 
+#define SD_CS       5
+#define SRAM_CS     6
+#define EPD_CS      9
+#define EPD_DC      10  
+#define EPD_RESET   -1 // can set to -1 and share with microcontroller Reset!
+#define EPD_BUSY    -1 // can set to -1 to not use a pin (will wait a fixed delay)
 
-#define EPD_CS     10
-#define EPD_DC      9
-#define SRAM_CS     -1
-#define EPD_RESET   5 // can set to -1 and share with microcontroller Reset!
-#define EPD_BUSY    6 // can set to -1 to not use a pin (will wait a fixed delay)
-
-//ThinkInk_154_Tricolor_Z17 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-//ThinkInk_213_Tricolor_RW display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-//ThinkInk_213_Tricolor_Z16 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-//ThinkInk_270_Tricolor_C44 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-//ThinkInk_290_Tricolor_Z10 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-ThinkInk_420_Tricolor_RW display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//ThinkInk_154_Mono_D67 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//ThinkInk_154_Mono_D27 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//ThinkInk_213_Mono_B72 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//ThinkInk_213_Mono_B73 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//ThinkInk_213_Mono_BN display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//ThinkInk_420_Mono_BN display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+ThinkInk_290_Grayscale4_T5 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) { delay(10); }
-  Serial.println("Adafruit EPD full update test in red/black/white");
-  display.begin(THINKINK_TRICOLOR);
+  Serial.println("Adafruit EPD full update test in mono");
+  display.begin(THINKINK_MONO);
 }
 
 void loop() {
   Serial.println("Banner demo");
   display.clearBuffer();
   display.setTextSize(3);
-  display.setCursor((display.width() - 144)/2, (display.height() - 24)/2);
+  display.setCursor((display.width() - 180)/2, (display.height() - 24)/2);
   display.setTextColor(EPD_BLACK);
-  display.print("Tri");  
-  display.setTextColor(EPD_RED);
-  display.print("Color");  
+  display.print("Monochrome");
   display.display();
 
-  delay(15000);
+  delay(2000);
   
-  Serial.println("Color rectangle demo");
+  Serial.println("B/W rectangle demo");
   display.clearBuffer();
-  display.fillRect(display.width()/3, 0, display.width()/3, display.height(), EPD_BLACK);
-  display.fillRect((display.width()*2)/3, 0, display.width()/3, display.height(), EPD_RED);
+  display.fillRect(display.width()/2, 0, display.width()/2, display.height(), EPD_BLACK);
   display.display();
 
-  delay(15000);
+  delay(2000);
   
   Serial.println("Text demo");
   // large block of text
@@ -58,7 +56,7 @@ void loop() {
   testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", EPD_BLACK);
   display.display();
 
-  delay(15000);
+  delay(2000);
 
   display.clearBuffer();
   for (int16_t i=0; i<display.width(); i+=4) {
@@ -66,11 +64,11 @@ void loop() {
   }
 
   for (int16_t i=0; i<display.height(); i+=4) {
-    display.drawLine(display.width()-1, 0, 0, i, EPD_RED);
+    display.drawLine(display.width()-1, 0, 0, i, EPD_BLACK);
   }
   display.display();
 
-  delay(15000);  
+  delay(2000);  
 }
 
 
