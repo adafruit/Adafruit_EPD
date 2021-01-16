ac77977c5237d71dbfdcd54233ec64873bac1aae
lady ada <limor@ladyada.net>
2021-01-16 18:00:38 -0500

    klong

--- src/Adafruit_EPD.cpp
+++ src/Adafruit_EPD.cpp
@@ -158,7 +158,7 @@ void Adafruit_EPD::begin(bool reset) {
     sram.write8(0, K640_SEQUENTIAL_MODE, MCPSRAM_WRSR);
   }
 
-  //Serial.println("set pins");
+  // Serial.println("set pins");
   // set pin directions
   pinMode(_dc_pin, OUTPUT);
   pinMode(_cs_pin, OUTPUT);
@@ -176,16 +176,16 @@ void Adafruit_EPD::begin(bool reset) {
     return;
   }
 
-  //Serial.println("hard reset");
+  // Serial.println("hard reset");
   if (reset) {
     hardwareReset();
   }
 
-  //Serial.println("busy");
+  // Serial.println("busy");
   if (_busy_pin >= 0) {
     pinMode(_busy_pin, INPUT);
   }
-  //Serial.println("done!");
+  // Serial.println("done!");
 }
 
 /**************************************************************************/
@@ -332,8 +332,8 @@ void Adafruit_EPD::writeSRAMFramebufferToEPD(uint16_t SRAM_buffer_addr,
     Serial.print("0x"); Serial.print((byte)c, HEX); Serial.print(", ");
     if (i % 32 == 31) {
       Serial.println();
-      Serial.print("$"); 
-      Serial.print(i, HEX); 
+      Serial.print("$");
+      Serial.print(i, HEX);
       Serial.print(": ");
     }
     */
