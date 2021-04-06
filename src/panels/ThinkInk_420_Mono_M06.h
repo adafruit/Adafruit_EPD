a214067a496f8e20a1866d6358feb4ff69127ba3
lady ada <limor@ladyada.net>
2021-04-06 11:03:13 -0400

    klangz

--- src/panels/ThinkInk_420_Mono_M06.h
+++ src/panels/ThinkInk_420_Mono_M06.h
@@ -7,12 +7,11 @@ class ThinkInk_420_Mono_M06 : public Adafruit_UC8276 {
 private:
 public:
   ThinkInk_420_Mono_M06(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST,
-                            int8_t CS, int8_t SRCS, int8_t MISO,
-                            int8_t BUSY = -1)
+                        int8_t CS, int8_t SRCS, int8_t MISO, int8_t BUSY = -1)
       : Adafruit_UC8276(300, 400, SID, SCLK, DC, RST, CS, SRCS, MISO, BUSY){};
 
   ThinkInk_420_Mono_M06(int8_t DC, int8_t RST, int8_t CS, int8_t SRCS,
-                            int8_t BUSY = -1, SPIClass *spi = &SPI)
+                        int8_t BUSY = -1, SPIClass *spi = &SPI)
       : Adafruit_UC8276(300, 400, DC, RST, CS, SRCS, BUSY, spi){};
 
   void begin(thinkinkmode_t mode = THINKINK_MONO) {
