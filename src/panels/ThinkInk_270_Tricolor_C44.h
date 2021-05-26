e158a3312b96f97e1c4a81f8aac8736c84844d68
hathach <thach@tinyusb.org>
2021-05-26 22:47:01 +0700

    fix missing BUSY in constructor

--- src/panels/ThinkInk_270_Tricolor_C44.h
+++ src/panels/ThinkInk_270_Tricolor_C44.h
@@ -27,11 +27,11 @@ public:
   ThinkInk_270_Tricolor_C44(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST,
                             int8_t CS, int8_t SRCS, int8_t MISO,
                             int8_t BUSY = -1)
-      : Adafruit_IL91874(264, 176, SID, SCLK, DC, RST, CS, SRCS, MISO, -1){};
+      : Adafruit_IL91874(264, 176, SID, SCLK, DC, RST, CS, SRCS, MISO, BUSY){};
 
   ThinkInk_270_Tricolor_C44(int8_t DC, int8_t RST, int8_t CS, int8_t SRCS,
                             int8_t BUSY = -1, SPIClass *spi = &SPI)
-      : Adafruit_IL91874(264, 176, DC, RST, CS, SRCS, -1, spi){};
+      : Adafruit_IL91874(264, 176, DC, RST, CS, SRCS, BUSY, spi){};
 
   void begin(thinkinkmode_t mode = THINKINK_TRICOLOR) {
     Adafruit_IL91874::begin(true);
