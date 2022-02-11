5efdafa036318db3791c501d2d5a1f3f9e043ffa
Melissa LeBlanc-Williams <melissa@adafruit.com>
2022-02-10 16:32:12 -0800

    Fix red inversion regardless of reset pin value

--- src/panels/ThinkInk_270_Tricolor_C44.h
+++ src/panels/ThinkInk_270_Tricolor_C44.h
@@ -35,8 +35,8 @@ public:
 
   void begin(thinkinkmode_t mode = THINKINK_TRICOLOR) {
     Adafruit_IL91874::begin(true);
-    setBlackBuffer(0, true);
-    setColorBuffer(1, true);
+    setBlackBuffer(0, _reset_pin < 0);
+    setColorBuffer(1, _reset_pin < 0);
 
     _epd_init_code = ti_270c44_tri_init_code;
     _epd_lut_code = NULL;
