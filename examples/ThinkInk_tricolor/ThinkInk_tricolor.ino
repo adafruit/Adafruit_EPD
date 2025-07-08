963343b28035ac8b0536a401e1f0e4df1fc6974b
ladyada <limor@ladyada.net>
2025-07-08 15:14:16 -0400

    biiig tricolor

--- examples/ThinkInk_tricolor/ThinkInk_tricolor.ino
+++ examples/ThinkInk_tricolor/ThinkInk_tricolor.ino
@@ -92,9 +92,11 @@ ThinkInk_213_Tricolor_RW display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY,
 // ThinkInk_420_Tricolor_MFGNR display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, 
 //                                     EPD_BUSY, EPD_SPI);
 
-// 5.83 Tricolor displays with 648 x 480 pixels and UC8179 chipset
+// 5.83 Tricolor displays with 648x480 pixels and UC8179 chipset
 // ThinkInk_583_Tricolor_AABMFGNR display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, EPD_SPI);
 
+// 7.5" Tricolor displays with 800x480 pixels and UC8179 chipset
+// ThinkInk_750_Tricolor_AABMFGNR display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY, EPD_SPI);
 
 
 void setup() {
--- src/panels/ThinkInk_583_Mono_AAAMFGN.h
+++ src/panels/ThinkInk_583_Mono_AAAMFGN.h
@@ -3,6 +3,30 @@
 
 #include "Adafruit_EPD.h"
 
+const uint8_t uc8179_mono_init_code[] {
+    UC8179_POWERSETTING, 4, 
+	0x07, // VGH=20V
+	0x07, // VGL=-20V
+	0x3F, // VDH=15V
+	0x3F, // VDL=-15V
+
+    UC8179_POWERON, 0,
+	0xFF, 100,          // busy wait
+	
+	UC8179_PANELSETTING, 1,
+	0b010111, // BW OTP LUT
+
+    UC8179_TRES, 4,
+	0x02, 0x88, 0x01, 0xE0,
+	
+	UC8179_DUALSPI, 1, 0x00,
+
+    UC8179_WRITE_VCOM, 2, 0x10, 0x07,
+    UC8179_TCON, 1, 0x22,
+
+    0xFE};
+
+
 class ThinkInk_583_Mono_AAAMFGN : public Adafruit_UC8179 {
  private:
  public:
@@ -19,6 +43,8 @@ class ThinkInk_583_Mono_AAAMFGN : public Adafruit_UC8179 {
     Adafruit_EPD::begin(true);
     setColorBuffer(1, false); // layer 1 uninverted
     setBlackBuffer(1, false); // only one buffer
+
+    _epd_init_code = uc8179_mono_init_code;
 
     layer_colors[EPD_WHITE] = 0b00;
     layer_colors[EPD_BLACK] = 0b01;
